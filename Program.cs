736d7007a7ea937dc6c4f1f50c2df10c89f33c4e
101669460 <61397176+101669460@users.noreply.github.com>
2020-08-16 15:08:08 +1000

    Completed Exceptions Task

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exceptions
+{
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            //Initialising variables
+            string RoomNum;
+            string Name;
+            DateTime Entered = new DateTime(1, 01, 01);
+            int TimeStayed = 0;
+
+            //User input variable values
+            RoomNum = Console.ReadLine();
+            Name = Console.ReadLine();
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            
+            //Handling Exceptions
+             try {
+                Entered = Convert.ToDateTime(input1);
+            } catch(Exception ex) {
+                Console.WriteLine($"Please enter a valid Date - {ex.Message}");
+            }
+
+            try {
+                TimeStayed = int.Parse(input2);
+            } catch(Exception ex) {
+                Console.WriteLine($"Please enter an integer - {ex.Message}");
+            }
+           
+            //Write variable values
+            Console.Write(RoomNum + ", " + Name + ", " + Entered + ", " + TimeStayed);
+        }
+
+    }   
+
+}
